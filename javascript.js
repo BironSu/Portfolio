96f0491ab083ac6b9d1253e405088afef3fdd7b3
Biron <bironsu@gmail.com>
2020-08-17 20:18:30 -0400

    Fixed Project description/Adding active scrolls

--- javascript.js
+++ javascript.js
@@ -1,28 +1,53 @@
 
 // MouseOver Grid
 const portfolioItems = document.querySelectorAll('.contact-item-wrapper');
+// Bind to scroll
+// Cache selectors
+var topMenu = $("#top-menu"),
+    topMenuHeight = topMenu.outerHeight() + 15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function () {
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+    });
 portfolioItems.forEach(portfolioItem => {
-    portfolioItem.addEventListener('mouseover', ()=> {
+    portfolioItem.addEventListener('mouseover', () => {
         portfolioItem.childNodes[1].classList.add('img-darken');
     })
-    portfolioItem.addEventListener('mouseout', ()=> {
+    portfolioItem.addEventListener('mouseout', () => {
         portfolioItem.childNodes[1].classList.remove('img-darken');
     })
 })
 
 // Intro Text Fade On Scroll
-$(window).scroll(function() {
+let headers = {
+    projectsID: document.getElementById("project-top"),
+    aboutID: document.getElementById("about-top"),
+    contactID: document.getElementById("contact-top")
+};
+function isHidden(el) {
+    return (el.offsetParent === null)
+}
+$(window).scroll(function () {
     var scrollTop = $(this).scrollTop();
     $('.intro-center-text').css({
-        opacity: function() {
-            opacity = 1 - (scrollTop*0.003);
+        opacity: function () {
+            opacity = 1 - (scrollTop * 0.003);
             return opacity;
         }
     })
+    if (!isHidden(headers.projectsID)){
+        const projectLink = menuItems.filter(function(_,menuItem) {
+            return menuItem.getAttribute('href') === '#project';
+        });
+        projectLink[0].classList.add("active-nav-link");
+    }
 });
 
 // Smooth Scrolling on Navigation
-window.smoothScroll = function(target) {
+window.smoothScroll = function (target) {
     var scrollContainer = target;
     do { //find scroll container
         scrollContainer = scrollContainer.parentNode;
@@ -36,28 +61,17 @@ window.smoothScroll = function(target) {
         targetY += target.offsetTop;
     } while (target = target.offsetParent);
 
-    scroll = function(c, a, b, i) {
+    scroll = function (c, a, b, i) {
         i++; if (i > 30) return;
         c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function(){ scroll(c, a, b, i); }, 20);
+        setTimeout(function () { scroll(c, a, b, i); }, 20);
     }
     // start scrolling
     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
-// Bind to scroll
-// Cache selectors
-var topMenu = $("#top-menu"),
-    topMenuHeight = topMenu.outerHeight()+15,
-    // All list items
-    menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function(){
-      var item = $($(this).attr("href"));
-      if (item.length) { return item; }
-});
 // Adding Active Link
 var previousLink;
-menuItems.click(function(e){
+menuItems.click(function (e) {
     var href = $(this).attr("href"),
         offsetTop = href === "#" ? 0 : $(href).offset();
     if (previousLink !== undefined) {
@@ -65,35 +79,35 @@ menuItems.click(function(e){
     }
     previousLink = this;
     this.classList.add("active-nav-link");
-    $('html, body').stop().animate({ 
+    $('html, body').stop().animate({
         scrollTop: offsetTop
     }, 300);
     e.preventDefault();
 });
-
-function showDescription(button){
+function showDescription(button) {
     var x = document.getElementById("project1-description");
     var y = document.getElementById("project2-description");
     var z = document.getElementById("project3-description");
-    if (button == "Button1"){
+    if (button == "Button1") {
         turnOn(x);
-        turnOff(y,z);
+        turnOff(y, z);
     } else if (button == "Button2") {
         turnOn(y);
-        turnOff(x,z);
+        turnOff(x, z);
     } else {
-       turnOn(z);
-       turnOff(x,y);
+        turnOn(z);
+        turnOff(x, y);
     }
 }
-function turnOn(desc){
-    desc.style.visibility = "visible";
-    desc.style.opacity = "1";
+function turnOn(desc) {
+    desc.classList.add("active");
 }
-function turnOff(desc1, desc2){
-    desc1.style.visibility = "hidden"
-    desc1.style.opacity = "0";
+function turnOff(desc1, desc2) {
+    desc1.className = "";
+    desc2.className = "";
 
-    desc2.style.visibility = "hidden"
-    desc2.style.opacity = "0";
+}
+
+function isHidden(el) {
+    return (el.offsetParent === null)
 }
